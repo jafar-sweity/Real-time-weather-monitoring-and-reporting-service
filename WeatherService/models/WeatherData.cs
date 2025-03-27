e0f96a165d806ca3a09de6be1ab0918a2a2a772d
jafar fuad <jata1518@gmail.com>
2025-03-27 23:18:06 +0200

    Add WeatherData class with Location, Temperature, and Humidity properties

---
+++ WeatherService/models/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherService.models
+{
+    public class WeatherData
+    {
+        public string location { get; set; }
+        public string temperature { get; set; }
+        public string humidity { get; set; }
+    }
+}
