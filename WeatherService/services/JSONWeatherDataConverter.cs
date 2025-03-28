6fd773727363e51cd48542e2ef3d665a81437208
jafar fuad <jata1518@gmail.com>
2025-03-28 03:14:13 +0300

    Added JSONWeatherDataConverter class implementing IWeatherDataConverter &  Added XMLWeatherDataConverter class implementing IWeatherDataConverter

---
+++ WeatherService/services/JSONWeatherDataConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WeatherService.interfaces;
+using WeatherService.models;
+
+namespace WeatherService.services
+{
+    class JSONWeatherDataConverter : IWeatherDataConverter
+    {
+        public WeatherData Convert(string input) => JsonSerializer.Deserialize<WeatherData>(input);
+    }
+}
---
+++ WeatherService/services/WeatherFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherService.interfaces;
+using WeatherService.models.Enum;
+
+namespace WeatherService.services
+{
+    public class WeatherFactory : IWeatherBotFactory
+    {
+        public IWeatherBot CreateBot(BotTypes botTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}
